8f89e16991fe2bdc974ca0d8fa155dae25c7549d
XavZol <alansolis707@gmail.com>
2026-04-16 07:51:50 -0600

    Consulta interactiva

--- Matplotlib_New.py
+++ Matplotlib_New.py
@@ -1,17 +1,65 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"C:\Users\javie\OneDrive\Desktop\Excel_DB\Iluminacion.csv")
-print(df)
+ruta = r"C:\Users\javie\OneDrive\Desktop\Excel_DB\Datos+Meteorológicos_Arg_2023.csv"
+df = pd.read_csv(ruta)
 
-# help(plt.hexbin)
+print(df.head())
 
-fig, axs = plt.subplots()
-hb = axs.hexbin(df['Latitud'], df['Longitud'], C=df['Luminosidad'], gridsize=50, cmap='Blues')
-axs.set_facecolor('#D2F2FF')
-plt.colorbar(hb, label="nivel de sombras")
-plt.xlabel("Latitud")
-plt.ylabel("Longitud")
-plt.title("Luminosidad en Valle")
-axs.set(xlim=(-4, 4), ylim=(-5,5))
-plt.show()
+df['Fecha'] = pd.to_datetime(df['Fecha'], format='%d/%m/%Y')
+print(df.head())
+
+
+lista_ciudades = []
+
+for c in df['Ciudad']:
+    if c not in lista_ciudades:
+        lista_ciudades.append(c)
+        print(lista_ciudades)
+
+dict_meses = { 1: 'Enero',
+                2: 'Febrero',
+                3: 'Marzo',
+                4: 'Abril',
+                5: 'Mayo',
+                6: 'Junio',
+                7: 'Julio',
+                8: 'Agosto',
+                9: 'Septiembre',
+                10: 'Octubre',
+                11: 'Noviembre',
+                12: 'Diciembre'}
+def consultar_temps():
+    while True:
+        # Solicitar la ciudad
+        print("\nCiudades disponibles: ", lista_ciudades)
+        ciudad_elejida = input("Elija la ciudad de la lista: ").title()
+
+        # Solicitar el mes
+        mes_elegido = int(input("Elija un mes (por ej. Enero = 1): "))
+
+        # Solicitar los inputs
+        if ciudad_elejida not in lista_ciudades or mes_elegido not in range(1, 13):
+            print("Ciudad o Mes no Válidos. Inténtelo Nuevamente.")
+            continue
+
+        # Crear el dataframe filtrando
+        datos_ciudad_mes = df[(df['Ciudad'] == ciudad_elejida) & (df['Fecha'].dt.month == mes_elegido)]
+
+        # Gráficamos las temperaturas del dataframe
+        plt.figure(figsize=(10, 6))
+        plt.plot(datos_ciudad_mes['Fecha'], datos_ciudad_mes['Temperatura Maxima'], label="Maxima", color="red")
+        plt.plot(datos_ciudad_mes['Fecha'], datos_ciudad_mes['Temperatura Minima'], label="Minima", color="blue")
+        plt.title(f"Temperaturas en {ciudad_elejida} durante el mes {dict_meses[mes_elegido]}")
+        plt.xlabel("Fecha")
+        plt.ylabel("Temperaturas")
+        plt.legend()
+        plt.xticks(rotation=45)
+        plt.show()
+
+        #Preguntar al usuario si quiere continuar
+        otra_consulta = input("¿Desea continuar consultando? (s/n): ")
+        if otra_consulta.lower() != 's':
+            break
+
+consultar_temps()
