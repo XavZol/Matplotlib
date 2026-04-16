fc9e939d82cf08697f62df9aaf25e939622f36e5
XavZol <alansolis707@gmail.com>
2026-04-16 06:53:01 -0600

    Estilos para los gráficos

--- Matplotlib_New.py
+++ Matplotlib_New.py
@@ -2,13 +2,36 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+df = pd.DataFrame({
+    "etiquetas":['Manzanas', 'Bananas', 'Naranjas', 'Uvas'],
+    "cantidad":[30,35, 26,33]
+})
+
+print(df)
+
 fig, axs = plt.subplots(ncols=2, nrows=1)
 axs1 = axs[0]
 axs2 = axs[1]
+axs1.plot(df['etiquetas'], df['cantidad'])
+axs2.pie(df['cantidad'])
+plt.show()
 
-axs1.plot([1, 2, 3, 4], [1, 4, 6, 8])
-axs2.scatter([1, 2, 3, 4], [1, 4, 6, 8])
+plt.style.use('ggplot')
 
-axs1.set_title("Gráfico de Línea")
-axs2.set_title("Gráfico Scatter")
+fig, axs = plt.subplots(ncols=2, nrows=1)
+axs1 = axs[0]
+axs2 = axs[1]
+axs1.plot(df['etiquetas'], df['cantidad'])
+axs2.pie(df['cantidad'])
+plt.show()
+
+print(plt.style.available)
+
+plt.style.use('seaborn-v0_8-pastel')
+
+fig, axs = plt.subplots(ncols=2, nrows=1)
+axs1 = axs[0]
+axs2 = axs[1]
+axs1.plot(df['etiquetas'], df['cantidad'])
+axs2.pie(df['cantidad'])
 plt.show()
