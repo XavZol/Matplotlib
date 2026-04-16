f58c77772b73bc76ad451daa8572cd4d9e5fba73
XavZol <alansolis707@gmail.com>
2026-04-16 05:02:52 -0600

    Creación de Gráficos

--- Matplotlib_New.py
+++ Matplotlib_New.py
@@ -1,26 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.DataFrame({
-    "etiquetas": ['Manzanas', 'Bananas', 'Naranjas', 'Uvas'],
-    "cantidad":[30, 60, 80, 20]
-})
-print(df)
 
-plt.pie(df['cantidad']);
-plt.show()
+fig, axs = plt.subplots(ncols=2, nrows=1)
+axs1 = axs[0]
+axs2 = axs[1]
 
-plt.pie(df['cantidad'], 
-        labels=df['etiquetas'],
-        autopct='%1.1f%%',
-        colors=['#87CEEB', '#6495ED', '#000080', '#40E0D0'])
-plt.show()
+axs1.plot([1, 2, 3, 4], [1, 4, 6, 8])
+axs2.scatter([1, 2, 3, 4], [1, 4, 6, 8])
 
-plt.pie(df['cantidad'], 
-        labels=df['etiquetas'],
-        autopct='%1.1f%%',
-        colors=['#87CEEB', '#6495ED', '#000080', '#40E0D0'],
-        shadow=True,
-        explode=(0,0,0, 0.5))
-plt.title("Mis Frutas") 
+axs1.set_title("Gráfico de Línea")
+axs2.set_title("Gráfico Scatter")
 plt.show()
