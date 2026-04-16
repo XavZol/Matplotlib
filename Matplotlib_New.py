0db65aed665591cf70b46ed981473a9d0dea12bd
XavZol <alansolis707@gmail.com>
2026-04-16 03:19:58 -0600

    Gráficos Scatter

--- Matplotlib_New.py
+++ Matplotlib_New.py
@@ -2,29 +2,36 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r"C:\Users\javie\OneDrive\Desktop\Excel_DB\Ventas.csv")
-print(df)
+x = np.linspace( 0, 10, 30)
+y = np.sin(x)
 
-plt.hist(df['Producto'])
+plt.plot(x, y, 'o') # Gráfico Scatter
+plt.show();
+
+aleatorio = np.random.RandomState(0)
+marcadores = ['o', 'x', '+', 'v', 's', 'd']
+for marcador in marcadores: 
+    plt.plot(aleatorio.rand(5), aleatorio.rand(5), marcador)
+    plt.show()
+    
+plt.plot(x,y , ':ob')
 plt.show()
 
-numeros = np.random.randn(1000)
-print(numeros)
-
-plt.hist(numeros)
+plt.plot(x, y, '-p')
 plt.show()
 
-plt.hist(numeros, bins=40, alpha=0.5, color='red')
-plt.show();
+plt.plot(x, y, '-p' ,color='red', markersize=15, linewidth=2, markerfacecolor='white')
+plt.show()
 
-plt.hist(numeros, bins=40, alpha=0.5, color='red', edgecolor='green', histtype='step')
-plt.show();
+plt.scatter(x, y)
+plt.show()
 
-
-x1 = np.random.randn(1000)
-x2 = np.random.randn(500)
-x3 = np.random.randn(100)
-plt.hist(x1, histtype='stepfilled', alpha=0.3, bins=40)
-plt.hist(x2, histtype='stepfilled', alpha=0.3, bins=40)
-plt.hist(x3, histtype='stepfilled', alpha=0.3, bins=40)
-plt.show();
+aleatorio1 = np.random.RandomState(0)
+plt.scatter(aleatorio1.randn(100),
+            aleatorio1.randn(100),
+            c=aleatorio1.randn(100),
+            s=aleatorio1.randn(100) * 1000,
+            alpha=0.3,
+            cmap='viridis')
+plt.colorbar()
+plt.show()
